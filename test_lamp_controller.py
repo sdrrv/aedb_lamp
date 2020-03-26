c4511218a14451fa4e8354124d46690b7841c092
André Sabino <705800+amgs@users.noreply.github.com>
2020-03-26 11:02:21 +0000

    Update tests.

--- test_lamp_controller.py
+++ test_lamp_controller.py
@@ -7,6 +7,17 @@ class TestLampController(unittest.TestCase):
         controller = LampController()
         controller.turn_on()
         self.assertTrue(controller.is_on())
+    
+    def test_turn_off(self):
+        controller = LampController()
+        controller.turn_off()
+        self.assertFalse(controller.is_on())
+    
+    def test_turn_off_after_turn_on(self):
+        controller = LampController()
+        controller.turn_on()
+        controller.turn_off()
+        self.assertFalse(controller.is_on())
 
 if __name__ == "__main__":
     unittest.main()
