94d40959fbd33f86efd1abdc70e7e7cd5f28846e
Duarte Elvas <duartecelvas@gmail.com>
2020-03-26 11:05:12 +0000

    Lamp

---
+++ controler.py
@@ -0,0 +1,13 @@
+from models import Lamp
+class LampController:
+    def __init__(self):
+        self.Lamp= Lamp()
+
+    def turn_on(self):
+        self.Lamp.set_on()
+
+    def turn_off(self):
+        self.Lamp.set_off()
+
+    def is_on(self):
+        return self.Lamp.get_state()
