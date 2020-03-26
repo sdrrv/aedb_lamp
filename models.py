09d2724ddc8a550baa23c13ae657c906e97aed87
Duarte Elvas <duartecelvas@gmail.com>
2020-03-26 12:10:17 +0000

    Color added

--- models.py
+++ models.py
@@ -8,6 +8,17 @@ class Lamp:
 
     def set_off(self):
         self.state=False
-        
+
     def get_state(self): #Não percebo o porque desta "Função", nao podiamos simplesmente chamar Lamp.state?
         return self.state
+
+class ColorLamp:
+
+    def __init__(self):
+        self.color="White" #Default Color
+    
+    def set_color(self,color):
+        self.color=color
+
+    def get_color(self):
+        return self.color
