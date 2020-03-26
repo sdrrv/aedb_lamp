83ae83ec3e6d33cccbb7d63a56ce84c075606e5e
monkey <m.b.rosario@hotmail.com>
2020-03-26 12:12:36 +0000

    a passar o novo teste

--- models.py
+++ models.py
@@ -8,6 +8,18 @@ class Lamp:
 
     def set_off(self):
         self.state=False
-        
+
     def get_state(self): #Não percebo o porque desta "Função", nao podiamos simplesmente chamar Lamp.state?
         return self.state
+
+class ColorLamp(Lamp):
+    def __init__(self):
+        Lamp.__init__(self)
+        self.color = "White"
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
