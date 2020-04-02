1efe2b41801dfcf8a55624a2a4943b2a707e3d76
André Sabino <705800+amgs@users.noreply.github.com>
2020-04-02 11:07:57 +0100

    Add missing test classes.

--- test_lamp_models.py
+++ test_lamp_models.py
@@ -1,8 +1,10 @@
 import unittest
 from models import ColorLamp
 
+class TestLamp(unittest.TestCase):
+    pass
+
 class TestColorLamp(unittest.TestCase):
-    
     def setUp(self):
         self.lamp = ColorLamp()
 
@@ -12,6 +14,9 @@ class TestColorLamp(unittest.TestCase):
     def test_set_color(self):
         self.lamp.set_color('Black')
         self.assertEqual(self.lamp.get_color(), 'Black')
+
+class TestLampArray(unittest.TestCase):
+    pass
 
 if __name__ == "__main__":
     unittest.main()
